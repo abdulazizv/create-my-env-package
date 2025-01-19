cb18dacae8adc76c3d0b5120ffe17ea5a812f4ba
Abdulaziz Bozorov <abdulazizheir@gmail.com>
2025-01-19 22:50:55 +0500

    Update config-variables.function.test.js

--- __tests__/config-variables.function.test.js
+++ __tests__/config-variables.function.test.js
@@ -6,7 +6,6 @@ describe('config-variables.function', () => {
     const tempEnvPath = path.join(process.cwd(), '.env');
 
     beforeAll(() => {
-        // Create a temporary .env file
         const envContent = `
 KEY1=value1
 KEY2=value2
@@ -19,22 +18,18 @@ KEY2=value2
     });
 
     beforeEach(() => {
-        // Clear environment variables before each test
         delete process.env.KEY1;
         delete process.env.KEY2;
     });
 
     afterEach(() => {
-        // Clear environment variables after each test
         delete process.env.KEY1;
         delete process.env.KEY2;
     });
 
     it('should load environment variables from .env file', () => {
-        // Load configuration
         config();
 
-        // Check environment variables
         expect(process.env.KEY1).toBe('value1');
         expect(process.env.KEY2).toBe('value2');
     });
